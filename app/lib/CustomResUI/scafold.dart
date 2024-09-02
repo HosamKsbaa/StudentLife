169a8ee063060bda813811d2647d4bd25480409b
Your Name <you@example.com>
2024-09-02 17:28:33 +0300

    test

--- app/lib/CustomResUI/scafold.dart
+++ app/lib/CustomResUI/scafold.dart
@@ -75,21 +75,43 @@ class _ResponsiveScaffoldState extends State<ResponsiveScaffold> {
         ].reversed.toList(),
       ),
       bottomNavigationBar: _buildResponsiveWidget(
-        smallScreenWidget: BottomNavigationBar(
+        smallScreenWidget:BottomNavigationBar(
           currentIndex: _currentIndex,
-          items: widget.navbarItems
-              .map((item) => BottomNavigationBarItem(
-                    icon: Icon(item.icon),
-                    label: item.text,
-                  ))
-              .toList(),
           onTap: (index) {
             setState(() {
               _currentIndex = index;
             });
             // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => widget.navbarItems[index].widget));
           },
-        ),
+          items: widget.navbarItems
+              .map((item) => BottomNavigationBarItem(
+            icon: Icon(
+              item.icon,
+              color: _currentIndex == widget.navbarItems.indexOf(item)
+                  ? Colors.blue // Active icon color
+                  : Colors.grey, // Inactive icon color
+            ),
+            label: item.text,
+          ))
+              .toList(),
+          backgroundColor: Colors.grey[100], // Background color of the navigation bar
+          selectedItemColor: Colors.blue, // Color of the selected item
+          unselectedItemColor: Colors.grey, // Color of unselected items
+          selectedLabelStyle: TextStyle(
+            fontWeight: FontWeight.bold, // Style for the selected label
+            fontSize: 14, // Increase font size for better readability
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontWeight: FontWeight.w400, // Style for the unselected label
+            fontSize: 12, // Slightly smaller font size for unselected items
+          ),
+          type: BottomNavigationBarType.fixed, // Ensures all items are always displayed
+          elevation: 10, // Add a subtle shadow for a more lifted look
+          showUnselectedLabels: true, // Display labels for unselected items
+          showSelectedLabels: true, // Display labels for selected items
+          iconSize: 28, // Increase icon size for better visibility
+        )
+        ,
         largeScreenWidget: null,
       ),
     );
